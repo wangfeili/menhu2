ebc4e1f8e1d1cdc5bf20427d507efb27072e26df
wangfeili <37933241+wangfeili@users.noreply.github.com>
2019-04-27 17:09:15 +0800

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+
+def index(request):
+
+    return render(request, 'base.html')
+
+
+def home(request):
+
+    return render(request, 'blog-home.html', locals())
+
